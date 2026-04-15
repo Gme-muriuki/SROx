a6138356dda616d177127ffce497ce66feb707cd
Awesome Rustacean <geniusinrust@gmail.com>
2026-04-15 17:24:19 +0300

    Implement the listener for running the client and the server.
    Handle transfer of data from client and the upstream by splitting
    bidirectional transfers
    Implement forwarding requests
    
    Add a main file which wires up everything implemented and run the proxy
    
    Create a config file with configuration loaded from the config module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello world!")
+use srox::{config::Config, listener};
+use std::{error::Error, path::Path, sync::Arc};
+use tracing_subscriber;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::fmt().json().init();
+
+    // Read config
+    let config = Config::load_from_file(Path::new("config.toml")).map_err(|err| {
+        tracing::error!(error = %err , "failed to read config");
+        err
+    })?;
+
+    let config = Arc::new(config);
+
+    tracing::info!("SROx starting");
+
+    listener::run(config).await?;
+
+    Ok(())
 }
